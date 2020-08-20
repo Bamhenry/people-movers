6b27111a9133cd41dae773c5b88b17c9dfb1ff26
Brooke McHenry <mchenry.bam@gmail.com>
2020-08-20 15:56:31 -0500

    added logo, pages, navigation, and created db

--- Site.Master.cs
+++ Site.Master.cs
@@ -7,6 +7,8 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using PeopleMovers.Models;
 
 namespace PeopleMovers
 {
@@ -70,6 +72,13 @@ namespace PeopleMovers
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new PeopleMovers.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
