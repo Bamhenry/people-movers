86604a932ae4f430a20ea4330b6e7728039bbfb7
Brooke McHenry <mchenry.bam@gmail.com>
2020-08-20 14:40:28 -0500

    created data model and db init and seed

---
+++ Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace PeopleMovers.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("PeopleMovers")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
