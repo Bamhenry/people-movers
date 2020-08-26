548d46f9fbf7c22e4a46e27ff6af1c820f65254c
Brooke McHenry <mchenry.bam@gmail.com>
2020-08-26 11:05:18 -0500

    added application-, page-, and code-level error logging

--- Global.asax.cs
+++ Global.asax.cs
@@ -47,12 +47,19 @@ namespace PeopleMovers
 
         void Application_Error(object sender, EventArgs e)
         {
+            // Code that runs when an unhandled error occurs.
+
+            // Get last error from the server
             Exception exc = Server.GetLastError();
 
             if (exc is HttpUnhandledException)
             {
-                // Pass the error on to the error page.
-                Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax", true);
+                if (exc.InnerException != null)
+                {
+                    exc = new Exception(exc.InnerException.Message);
+                    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+                        true);
+                }
             }
         }
     }
